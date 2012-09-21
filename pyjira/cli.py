4053642c3dbe749645a42c3b0fda71c15512af27
kijasek <kijasek@gmail.com>
2012-09-21 13:29:47 +0200

    command line argument parsing

--- pyjira/cli.py
+++ pyjira/cli.py
@@ -1,4 +1,5 @@
 # -*- coding: utf8 -*-
+import argparse
 import getpass
 import sys
 import os
@@ -6,16 +7,34 @@ from ConfigParser import RawConfigParser
 
 from pyjira.core import JiraClient
 
-def main():
+def get_config():
     conf_dir = os.path.dirname(__file__)
     conf_path = os.path.join(conf_dir, 'pyjira.conf')
     config_parser = RawConfigParser()
     config_parser.read([conf_path])
 
-    server = config_parser.get('jira settings', 'host')
-    username = config_parser.get('jira settings', 'username')
-    password = config_parser.get('jira settings', 'password')
-    project = config_parser.get('jira settings', 'default_project')
+    return config_parser
+
+def create_argparse():
+    parser = argparse.ArgumentParser(description='CLI for common JIRA tasks')
+    subparsers = parser.add_subparsers(title='valid actions', dest="action")
+
+    list_my_issues_parser = subparsers.add_parser('my',
+                                             help='List issues assigned to me')
+    list_my_issues_parser.add_argument('-p', '--project', 
+                                     help='Only issues from specified project')
+    list_my_issues_parser.add_argument('-u', '--unresolved', 
+                                     action='store_true',
+                                     help='Issues that have unresolved status')
+
+    return parser
+
+def main():
+    config = get_config()
+    server = config.get('jira settings', 'host')
+    username = config.get('jira settings', 'username')
+    password = config.get('jira settings', 'password')
+    project = config.get('jira settings', 'default_project')
 
     if not server:
         sys.exit('Please specify valid server address in pyjira.conf')
@@ -25,7 +44,11 @@ def main():
         password = getpass.getpass()
     if project.strip() == '':
         project = None
-    
+   
+    parser = create_argparse()
+    args = parser.parse_args()
+    args = vars(args)
     jira_client = JiraClient(server, username, password, project)
-    jira_client.list_my_issues()
+    method_to_call = getattr(jira_client, args['action'])
+    method_to_call(**args)
 
--- pyjira/core.py
+++ pyjira/core.py
@@ -3,27 +3,33 @@ from jira.client import JIRA
 
 class JiraClient(object):
     
-    def __init__(self, server, username, password, project=None):
+    def __init__(self, server, username, password, default_project=None):
         self.username = username
         self.password = password 
-        self.project = project 
+        self.default_project = default_project 
         self.server = server
 
         self.jira = JIRA(options={'server': self.server},
                          basic_auth=(self.username, self.password))
 
-    def list_my_issues(self):
-        query = 'project=%s and assignee=%s' % (self.project, self.username)
+    def my(self, **kwargs):
+        self._list_my_issues(**kwargs)
+
+    def _list_my_issues(self, **kwargs):
+        query_parts = ['assignee=%s' % (self.username)]
+
+        if kwargs['project'] is not None: 
+            query_parts.append('project=%s' % kwargs['project'])
+        elif self.default_project is not None:
+            query_parts.append('project=%s' % self.default_project)
+
+        if kwargs['unresolved']:
+            query_parts.append('resolution=unresolved')
+        
+        query = ' and '.join(query_parts)      
         my_issues = self.jira.search_issues(query)
 
         for issue in my_issues:
             print '%s\t%s' % (issue.key, issue.fields.summary)
 
-    def list_my_current_issues(self):
-        query = 'project=%s and assignee=%s and resolution=unresolved' % (
-                                                   self.project, self.username)
-        my_issues = self.jira.search_issues(query)
-
-        for issue in my_issues:
-            print '%s\t%s' % (issue.key, issue.fields.summary)
 
